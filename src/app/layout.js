610f67b3740e0acd7ea63c5af12bd2dad445c695
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2023-09-14 03:51:56 +0530

    Added MUI

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import ThemeRegistry from '@/mui/ThemeRegistry/ThemeRegistry'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <body className={inter.className}>
+      <ThemeRegistry options={{ key: "mui" }}>
+        {children}
+      </ThemeRegistry>
+    </body>
+  </html>
   )
 }
