3210bfab772b1b17904f82558ce776f2bff38609
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2023-09-22 02:28:42 +0530

    Refactored and fixed Code

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,64 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Alert, AlertTitle, Box, Button, Drawer, Grid, List, ListItem, Typography } from '@mui/material'
-import MediaCard from '@/components/MediaCard'
-import Footer from '@/components/Footer'
+import Footer from "@/components/Footer";
+import styles from "./page.module.css";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box sx={{
-      maxHeight: '100vh',
-      minHeight: '100%',
-      // background: 'radial-gradient(circle, #ccfcec, #eafccc, #effabb)'
-      // background: '#68E498',
-      background: 'radial-gradient(circle, rgba(104,228,152,0.4), rgba(255,255,255,1))'
-    }}>
-      
-      <Typography variant="h1" sx={{ textAlign: 'center', paddingTop:'10rem' }}>ZENiTH</Typography>
-      <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>Drag n Drop Interactive ML Model Maker</Typography>
-      
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2rem', paddingBottom: '14rem' }}>
-        
-        <Button variant="outlined" color="primary" sx={{ marginRight: '1rem' }}>Get Started</Button>
-        <Button variant="outlined" color="primary" sx={{ marginRight: '1rem' }}>Button 2</Button>
-        <Button variant="outlined" color="primary" sx={{ marginRight: '1rem' }}>Button 3</Button>
-      
+    <Box
+      sx={{
+        maxHeight: "100vh",
+        minHeight: "100vh",
+        background:
+          "radial-gradient(circle, rgba(104,228,152,0.4), rgba(255,255,255,1))",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box>
+        <Typography
+          variant="h1"
+          sx={{ textAlign: "center", paddingTop: "10rem" }}
+        >
+          ZENiTH
+        </Typography>
+        <Typography variant="subtitle1" sx={{ textAlign: "center" }}>
+          Drag n Drop Interactive ML Model Maker
+        </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "2rem",
+            paddingBottom: "14rem",
+          }}
+        >
+          <Button
+            variant="outlined"
+            color="primary"
+            // sx={{ marginRight: "1rem" }}
+          >
+            Get Started
+          </Button>
+          {/* <Button
+            variant="outlined"
+            color="primary"
+            sx={{ marginRight: "1rem" }}
+          >
+            Button 2
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            sx={{ marginRight: "1rem" }}
+          >
+            Button 3
+          </Button> */}
+        </Box>
       </Box>
+      <Footer />
     </Box>
-  )
+  );
 }
