d4b4e618793df38e6e9ded16f446b8804e0ebd70
Aarav Sharma <mailaarav2002@gmail.com>
2023-09-21 21:37:39 +0530

    Inital homepage design

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import ThemeRegistry from '@/mui/ThemeRegistry/ThemeRegistry'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +16,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
     <body className={inter.className}>
       <ThemeRegistry options={{ key: "mui" }}>
+        <Navbar />
         {children}
+        <Footer />
       </ThemeRegistry>
     </body>
   </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,29 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Typography } from '@mui/material'
+import { Alert, AlertTitle, Box, Button, Drawer, Grid, List, ListItem, Typography } from '@mui/material'
+import MediaCard from '@/components/MediaCard'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-   <div>
-    <Typography variant="h1">ZENITH: Drag and drop</Typography>
-   </div>
+    <Box sx={{
+      maxHeight: '100vh',
+      minHeight: '100%',
+      // background: 'radial-gradient(circle, #ccfcec, #eafccc, #effabb)'
+      // background: '#68E498',
+      background: 'radial-gradient(circle, rgba(104,228,152,0.4), rgba(255,255,255,1))'
+    }}>
+      
+      <Typography variant="h1" sx={{ textAlign: 'center', paddingTop:'10rem' }}>ZENiTH</Typography>
+      <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>Drag n Drop Interactive ML Model Maker</Typography>
+      
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2rem', paddingBottom: '14rem' }}>
+        
+        <Button variant="outlined" color="primary" sx={{ marginRight: '1rem' }}>Get Started</Button>
+        <Button variant="outlined" color="primary" sx={{ marginRight: '1rem' }}>Button 2</Button>
+        <Button variant="outlined" color="primary" sx={{ marginRight: '1rem' }}>Button 3</Button>
+      
+      </Box>
+    </Box>
   )
 }
