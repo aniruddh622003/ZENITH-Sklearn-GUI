34fb4ffc887eb33410574ee4f543c998e7d8a269
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2023-09-22 23:17:25 +0530

    Created Layout file in /app/flow directory

---
+++ src/app/flow/layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FlowLayout = ({ children }) => {
+  return (
+    <div>
+      Add Stepper Here
+      <br />
+      {children}
+    </div>
+  );
+};
+
+export default FlowLayout;
