ec7f3e55b4308a0d784496f0ef11659668cdc4d9
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2023-09-26 04:31:27 +0530

    Minor Tweaks

--- src/app/flow/upload-dataset/page.jsx
+++ src/app/flow/upload-dataset/page.jsx
@@ -1,10 +1,10 @@
 "use client";
-import UploadIcon from '@mui/icons-material/Upload';
+import UploadIcon from "@mui/icons-material/Upload";
 import { ArrowForwardIos } from "@mui/icons-material";
 import Upload from "@/components/Upload";
 import React from "react";
 import styles from "./index.module.css";
-import { Button } from '@mui/material';
+import { Box, Button } from "@mui/material";
 
 const UploadDataset = () => {
   const upload = (file) => {
@@ -13,24 +13,32 @@ const UploadDataset = () => {
 
   return (
     <div className={styles.page}>
-      <head>
-        <title>ZENiTH - Upload Dataset</title>
-      </head>
       <div className={styles.container}>
         <div className={styles.heading}>
-          <h1>
-            Upload Dataset <UploadIcon className={styles.icon} />
-          </h1>
-          <p className={styles.quote}>Data is a precious thing and will last longer than the systems themselves.</p>
+          <h1>Upload Dataset</h1>
+          <p className={styles.quote}>
+            Data is a precious thing and will last longer than the systems
+            themselves.
+          </p>
           <p className={styles.quote}>- Tim Berners-Lee</p>
         </div>
-        <div className={styles.line}></div>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+          }}
+          className={styles.line}
+        ></Box>
         <div className={styles.upload}>
           <Upload onSubmit={upload} />
         </div>
       </div>
       <div className={styles.next}>
-        <Button variant="outlined" color="primary" endIcon={<ArrowForwardIos />}>
+        <Button
+          variant="outlined"
+          color="primary"
+          disabled={true}
+          endIcon={<ArrowForwardIos />}
+        >
           Next
         </Button>
       </div>
