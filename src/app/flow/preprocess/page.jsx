3a7a612a97c074e78a971dd819169ebe09209be9
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2023-10-01 03:51:35 +0530

    started working

--- src/app/flow/preprocess/page.jsx
+++ src/app/flow/preprocess/page.jsx
@@ -1,10 +1,18 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const Preprocess = () => {
   return (
-    <div>
+    <Box
+      sx={{
+        backgroundColor: "primary.main",
+        height: "100%",
+        m: 4,
+        mt: 0,
+      }}
+    >
       <h1>Preprocessing Page</h1>
-    </div>
+    </Box>
   );
 };
 
