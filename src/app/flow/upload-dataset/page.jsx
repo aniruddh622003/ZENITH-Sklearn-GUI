6ad51afc5dafd8504d6548c37756863de4eb0f80
Khushi Gupta <guptakhushi5555@gmail.com>
2023-10-24 19:12:30 +0530

    Popup on Existing dataset (dra-33) done

--- src/app/flow/upload-dataset/page.jsx
+++ src/app/flow/upload-dataset/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 import { ArrowForwardIos } from "@mui/icons-material";
 import Upload from "@/components/Upload";
-import React, { useEffect } from "react";
-import Grid from '@mui/material/Grid';
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
 
 import {
   Alert,
@@ -12,6 +12,7 @@ import {
   Collapse,
   Snackbar,
   Typography,
+  Modal,
 } from "@mui/material";
 
 const UploadDataset = () => {
@@ -28,6 +29,95 @@ const UploadDataset = () => {
     error: false,
     message: "",
   });
+
+  const [showModal, setShowModal] = useState(true);
+  const [data, setData] = useState(false);
+  const fetchInfo = () => {
+    return fetch("/api/existing-dataset");
+  };
+  useEffect(() => {
+    fetchInfo()
+      .then(async (response) => {
+        console.log(response.status);
+        if (response.status == 200) {
+          let dataF = await response.json();
+          console.log(dataF);
+          setData(dataF);
+          setShowModal(true);
+        } else {
+          //error
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const modalContent = (
+    <Box
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 500,
+        bgcolor: "primary.bg",
+        border: "2px solid #000",
+        boxShadow: 30,
+        borderRadius: "20px",
+        p: 3.5,
+      }}
+    >
+      <Typography variant="h4" p="2px" sx={{ fontWeight: "bold" }}>
+        Dataset Already Exists
+      </Typography>
+      <hr
+        style={{
+          margin: "10px 0",
+          height: "2px",
+          bgcolor: "primary.bg",
+          pt: "2px",
+        }}
+      />
+      <Typography p="3px">
+        Dataset:<span style={{ fontWeight: "bold" }}>{data.filename}</span>
+      </Typography>
+      <Typography pb="7px">
+        Rows: <span style={{ fontWeight: "bold" }}>{data.rows}&nbsp;</span>
+        <br />
+        Columns: <span style={{ fontWeight: "bold" }}>{data.columns}</span>
+      </Typography>
+      <hr
+        style={{
+          margin: "7px 0",
+          paddingBottom: "5px",
+          height: "2px",
+          bgcolor: "primary.bg",
+        }}
+      />
+      <Button
+        variant="contained"
+        color="error"
+        onClick={closeModal}
+        sx={{ mr: "15px" }}
+      >
+        Overwrite
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          window.location.href = "/flow/preprocess";
+        }}
+      >
+        Navigate
+      </Button>
+    </Box>
+  );
   const handleNextClick = () => {
     if (uploaded) {
       window.location.href = "/flow/preprocess";
@@ -62,15 +152,27 @@ const UploadDataset = () => {
   };
 
   return (
-    <Grid sx={{height: '100%'}}>
-      <Grid container sx={{ alignItems: 'center', height: '90%'}}>
-        <Grid sx={{flex: '1', paddingLeft: '5rem', paddingRight: '2rem'}}>
+    <Grid sx={{ height: "100%" }}>
+      <Grid container sx={{ alignItems: "center", height: "90%" }}>
+        <Grid sx={{ flex: "1", paddingLeft: "5rem", paddingRight: "2rem" }}>
           <Typography variant="h2">Upload Dataset</Typography>
-          <Typography variant="h5">Data is a precious thing and will last longer than the systems themselves.</Typography>
-          <Typography variant="h5" align="right">- Tim Berners-Lee</Typography>
+          <Typography variant="h5">
+            Data is a precious thing and will last longer than the systems
+            themselves.
+          </Typography>
+          <Typography variant="h5" align="right">
+            - Tim Berners-Lee
+          </Typography>
         </Grid>
-        <Box sx={{backgroundColor: "primary.main", width: "6px", height: "75%", borderRadius: '3px'}}></Box>
-        <Grid sx={{flex: '1', paddingLeft: '2rem', paddingRight: '5rem'}}>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            width: "6px",
+            height: "75%",
+            borderRadius: "3px",
+          }}
+        ></Box>
+        <Grid sx={{ flex: "1", paddingLeft: "2rem", paddingRight: "5rem" }}>
           <Collapse in={snackProgress.uploaded} sx={{ mb: "20px" }}>
             <Alert
               severity="success"
@@ -89,7 +191,9 @@ const UploadDataset = () => {
           <Upload onSubmit={upload} />
         </Grid>
       </Grid>
-      <Grid sx={{display: 'flex', justifyContent: 'end', paddingRight: '5rem'}}>
+      <Grid
+        sx={{ display: "flex", justifyContent: "end", paddingRight: "5rem" }}
+      >
         <Button
           variant="outlined"
           color="primary"
@@ -154,6 +258,7 @@ const UploadDataset = () => {
           {err.message}
         </Alert>
       </Snackbar>
+      <Modal open={showModal}>{modalContent}</Modal>
     </Grid>
   );
 };
