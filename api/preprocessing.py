4e2e1a9ee844a412c8c3bf06772c5ea4380c9338
Aarav Sharma <mailaarav2002@gmail.com>
2023-12-03 22:59:24 +0530

    added functionality to save pre and post results as png

--- api/preprocessing.py
+++ api/preprocessing.py
@@ -1,8 +1,13 @@
+import matplotlib
+matplotlib.use('Agg')
+
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, Normalizer, OneHotEncoder, OrdinalEncoder, LabelEncoder, PolynomialFeatures
 from sklearn.impute import SimpleImputer
 import pandas as pd
 import numpy as np
 import ast
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 # this dictionary contains the preprocessing objects
 preprocess_dict = {
@@ -77,7 +82,48 @@ def apply_preprocessing(df, selected_preprocessing):
             df[allowed_columns] = pd.DataFrame(preprocess.fit_transform(df[allowed_columns]))
     return df
 
+def save_sf_snapshot(df, loc='api/outputs/', label='default'):
+    df_head = df.head(8)
+    df_describe = df.describe().round(4).reset_index()  # round to 4 decimal places
+
+    fig = plt.figure(figsize=(13, 2))
+
+    gs = gridspec.GridSpec(1, 2, width_ratios=[5, 6])  # specify width ratios
+
+    ax0 = plt.subplot(gs[0])
+    ax1 = plt.subplot(gs[1])
+
+    # Hide axes
+    ax0.axis('off')
+    ax1.axis('off')
+
+    # Plot df_head
+    tabla1 = ax0.table(cellText=df_head.values, colLabels=df_head.columns, cellLoc = 'center', loc='center')
+    tabla1.auto_set_font_size(False)
+    tabla1.set_fontsize(10)
+    tabla1.scale(1.2, 1.2)
+
+    # Change color of first row
+    for i in range(len(df_head.columns)):
+        tabla1.get_celld()[(0, i)].set_facecolor('#98FB98')  # light green
+
+    # Plot df_describe
+    tabla2 = ax1.table(cellText=df_describe.values, colLabels=df_describe.columns, cellLoc = 'center', loc='center')
+    tabla2.auto_set_font_size(False)
+    tabla2.set_fontsize(10)
+    tabla2.scale(1.2, 1.2)
+
+    # Change color of first row
+    for i in range(len(df_describe.columns)):
+        tabla2.get_celld()[(0, i)].set_facecolor('#98FB98')  # light green
+
+    plt.subplots_adjust(wspace=0.25)  # add spacing between subplots
+
+    plt.savefig(f'{loc}df_{label}.png')
+
 def apply_pipeline(preprocess_pipeline):
     df = pd.read_csv('api/uploads/data.csv')
+    save_sf_snapshot(df, 'api/outputs/', 'pre')
     df = apply_preprocessing(df, preprocess_pipeline)
+    save_sf_snapshot(df, 'api/outputs/', 'post')
     df.to_csv('api/uploads/data_preprocessed.csv', index=False)
