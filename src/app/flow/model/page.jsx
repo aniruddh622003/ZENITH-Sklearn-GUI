65b2e80f2f7b8e1f8b8ec73f0907ebc0375de4a3
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2023-12-05 03:49:32 +0530

    Code Helper Modal Opens after downloading model weights

--- src/app/flow/model/page.jsx
+++ src/app/flow/model/page.jsx
@@ -13,15 +13,33 @@ import {
   Tooltip,
   CircularProgress,
   Collapse,
+  Modal,
+  Typography,
 } from "@mui/material";
 import TextBox from "@/components/TextBox";
 import { HiInformationCircle } from "react-icons/hi";
 import {
+  Close,
   OnlinePredictionSharp,
   StayPrimaryLandscape,
 } from "@mui/icons-material";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
+import { CodeBlock, CopyBlock, dracula } from "react-code-blocks";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "75%",
+  bgcolor: "background.paper",
+  border: "none",
+  boxShadow: 24,
+  p: 4,
+  outline: "none",
+  borderRadius: "15px",
+};
 
 const options = [
   "LinearRegression",
@@ -50,6 +68,7 @@ const ModelPage = () => {
   const [selectedModelParameters, setSelectedModelParameters] = useState(null);
   const [graph, setGraph] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [codeHelper, setCodeHelper] = useState(false);
 
   useEffect(() => {
     async function fetchParam() {
@@ -125,6 +144,9 @@ const ModelPage = () => {
         method: "GET",
       });
       // Make a downloadable as file
+      if (resp.status != 200) {
+        return;
+      }
       const blob = await resp.blob();
       const url = window.URL.createObjectURL(blob);
       const a = document.createElement("a");
@@ -133,6 +155,7 @@ const ModelPage = () => {
       document.body.appendChild(a);
       a.click();
       a.remove();
+      setCodeHelper(true);
     } catch (error) {
       console.error(error);
     }
@@ -140,6 +163,47 @@ const ModelPage = () => {
 
   return (
     <Grid container spacing={4}>
+      <Modal open={codeHelper} onClose={() => setCodeHelper(false)}>
+        <Box sx={style}>
+          <Box sx={{ display: "flex" }}>
+            <Box>
+              <Typography variant="h6">
+                <b>You have downloaded the model weights!</b>
+                <br />
+                To use the model weights in your code, you can use the following
+                code snippet as a reference:
+              </Typography>
+            </Box>
+            <Box sx={{ ml: "auto" }}>
+              <IconButton onClick={() => setCodeHelper(false)}>
+                <Close />
+              </IconButton>
+            </Box>
+          </Box>
+          <br />
+          <CopyBlock
+            language="python"
+            text={`from joblib import load
+
+model_path = "my_model.joblib" # path to your model weights
+X = [[5.7,2.9,4.2,1.3],
+        [6.2,2.9,4.3,1.3],
+        [5.1,2.5,3.0,1.1]] # input data (you can also use pandas dataframe)
+
+def load_model(model_path):
+    model = load(model_path)
+    return model
+
+def predict(model_path, X):
+    model = load_model(model_path)
+    y_pred = model.predict(X)
+    return y_pred`}
+            showLineNumbers={true}
+            theme={dracula}
+            codeBlock={{ lineNumbers: false, wrapLines: true }}
+          />
+        </Box>
+      </Modal>
       <Grid item xs={6}>
         <Box
           display="flex"
